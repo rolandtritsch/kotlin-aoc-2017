5ec07a2d119a1014fb5b12171d035b0cfe6c72f8
Roland Tritsch <roland@tritsch.org>
2018-05-28 21:09:19 +0100

    Add HOME to make it work on my Mac and Ubuntu

--- src/main/kotlin/aoc/Util.kt
+++ src/main/kotlin/aoc/Util.kt
@@ -2,7 +2,10 @@ package aoc
 
 object Util {
   fun readInput(fileName: String): List<String> {
-    val fqn = "/Users/rtritsch/Development/Home/kotlin-aoc-2017/src/main/resources" + "/" + fileName
+    val home = System.getenv("HOME")
+    val devDir = "/Development/Home/kotlin-aoc-2017"
+    val resDir = "/src/main/resources"
+    val fqn = "${home}/${devDir}/${resDir}/${fileName}"
     val lines = java.io.File(fqn).readLines()
     return lines
   }
