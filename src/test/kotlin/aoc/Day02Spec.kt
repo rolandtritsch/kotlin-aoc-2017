f0f3d4779fa98b782d4fb8536a8b428c00558658
Roland Tritsch <roland@tritsch.org>
2018-05-30 20:24:03 +0100

    Day02: Added (commented out) the correct test results.

--- src/test/kotlin/aoc/Day02Spec.kt
+++ src/test/kotlin/aoc/Day02Spec.kt
@@ -5,12 +5,14 @@ import io.kotlintest.specs.ShouldSpec
 
 class Day02Spec : ShouldSpec({
   "solve()" should {
-    "solve part1" {
+    "solve Part1" {
       Day02.Part1.solve(Day02.input) shouldBe 0
+      //Day02.Part1.solve(Day02.input) shouldBe 34925
     }
 
-    "solve part2" {
+    "solve Part2" {
       Day02.Part2.solve(Day02.input) shouldBe 0
+      //Day02.Part2.solve(Day02.input) shouldBe 221
     }
   }
 })
