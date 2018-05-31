8a7e330c299dab0d0173dcc7f54f77e09cc0f851
Roland Tritsch <rtritsch@tenable.com>
2018-05-31 15:17:17 +0100

    Making gradle test work

--- src/test/kotlin/aoc/Day02Spec.kt
+++ src/test/kotlin/aoc/Day02Spec.kt
@@ -5,14 +5,12 @@ import io.kotlintest.specs.ShouldSpec
 
 class Day02Spec : ShouldSpec({
   "solve()" should {
-    "solve Part1" {
-      Day02.Part1.solve(Day02.input) shouldBe 0
-      //Day02.Part1.solve(Day02.input) shouldBe 34925
+    "solve part1" {
+      Day02.Part1.solve(Day02.input) shouldBe 34925
     }
 
-    "solve Part2" {
-      Day02.Part2.solve(Day02.input) shouldBe 0
-      //Day02.Part2.solve(Day02.input) shouldBe 221
+    "solve part2" {
+      Day02.Part2.solve(Day02.input) shouldBe 221
     }
   }
 })
