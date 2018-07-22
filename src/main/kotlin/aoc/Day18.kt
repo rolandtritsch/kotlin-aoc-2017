5d02cddf7bee5dae797b95bbbf08569f0f69af2f
Roland Tritsch <roland@tritsch.org>
2018-07-22 13:06:01 +0100

    Day18: Checkpoint. Compiles. Tests failing.

--- src/main/kotlin/aoc/Day18.kt
+++ src/main/kotlin/aoc/Day18.kt
@@ -1,4 +1,7 @@
-package aoc
+package aoc.day18
+
+import java.util.concurrent.LinkedBlockingDeque
+import java.util.concurrent.TimeUnit
 
 /** Problem: [[https://adventofcode.com/2017/day/18]]
   *
@@ -30,289 +33,286 @@ package aoc
   */
 object Day18 {
 
-  val input = Util.readInput("Day18input.txt")
+  val input = aoc.Util.readInput("Day18input.txt")
 
-  def parseInput(input: List[String]): List[Operation] = {
-    require(input.nonEmpty, s"input.nonEmpty failed")
-
-    val registerRange = ('a'to('z')).toList
-
-    input.map(l => {
-      val tokens = l.split(' ')
-      assert(tokens.size >= 2)
-      val operation = tokens(0)
-      val register = tokens(1).charAt(0)
-      assert(registerRange.contains(register))
-      operation match {
-        case "snd" => Send(register)
-        case "set" => {
-          assert(tokens.size == 3)
-          val operand = tokens(2)
-          if (registerRange.contains(operand.charAt(0))) SetR(register, operand.charAt(0))
-          else Set(register, operand.toLong)
-        }
-        case "add" => {
-          assert(tokens.size == 3)
-          val operand = tokens(2)
-          if (registerRange.contains(operand.charAt(0))) AddR(register, operand.charAt(0))
-          else Add(register, operand.toLong)
-        }
-        case "mul" => {
-          assert(tokens.size == 3)
-          val operand = tokens(2)
-          if (registerRange.contains(operand.charAt(0))) MultiplyR(register, operand.charAt(0))
-          else Multiply(register, operand.toLong)
-        }
-        case "mod" => {
-          assert(tokens.size == 3)
-          val operand = tokens(2)
-          if (registerRange.contains(operand.charAt(0))) ModuloR(register, operand.charAt(0))
-          else Modulo(register, operand.toLong)
-        }
-        case "rcv" => Receive(register)
-        case "jgz" => {
-          assert(tokens.size == 3)
-          val operand = tokens(2)
-          if (registerRange.contains(operand.charAt(0))) JumpIfGreaterThanZeroR(register, operand.charAt(0))
-          else JumpIfGreaterThanZero(register, operand.toInt)
-        }
-        case _ => assert(false); Send('a')
-      }
-    })
-  } ensuring(_.nonEmpty, s"_.nonEmpty failed")
-
-  import java.util.concurrent.{LinkedBlockingDeque, TimeUnit}
-  case class Program(
-    id: Int,
-    counter: Int,
-    instructions: List[Operation],
-    register: Map[Char, Long],
-    readChannel: LinkedBlockingDeque[Long],
-    writeChannel: LinkedBlockingDeque[Long],
-    writeCount: Int,
-    checkRegisterOnReceive: Boolean,
-    deadlocked: Boolean
+  data class Program(
+    val id: Int,
+    val counter: Int,
+    val instructions: List<Operation>,
+    val register: MutableMap<Char, Long>,
+    val readChannel: LinkedBlockingDeque<Long>,
+    val writeChannel: LinkedBlockingDeque<Long>,
+    val writeCount: Int,
+    val checkRegisterOnReceive: Boolean,
+    val deadlocked: Boolean
   )
 
-  sealed abstract class Operation {
-    def execute(program: Program): Program
-  }
+  sealed class Operation {
+    abstract fun execute(program: Program): Program
 
-  case class Send(r: Char) extends Operation {
-    def execute(program: Program): Program = {
-      val success = program.writeChannel.offerFirst(program.register(r))
-      assert(success)
-      program.copy(
-        counter = program.counter + 1,
-        writeCount = if(program.id == 1) program.writeCount + 1 else program.writeCount
-      )
+    data class Send(val r: Char) : Operation() {
+      override fun execute(program: Program): Program {
+        val success = program.writeChannel.offerFirst(program.register.get(r))
+        assert(success)
+        return program.copy(
+          counter = program.counter + 1,
+          writeCount = if(program.id == 1) program.writeCount + 1 else program.writeCount
+        )
+      }
     }
-  }
 
-  case class Set(r: Char, v: Long) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> v)
-      )
+    data class Set(val r: Char, val v: Long) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = v
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class SetR(r: Char, v: Char) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> program.register(v))
-      )
+    data class SetR(val r: Char, val v: Char) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = program.register.getValue(r)
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class Add(r: Char, v: Long) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> (program.register(r) + v))
-      )
+    data class Add(val r: Char, val v: Long) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = program.register.getValue(r) + v
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class AddR(r: Char, v: Char) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> (program.register(r) + program.register(v)))
-      )
+    data class AddR(val r: Char, val v: Char) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = program.register.getValue(r) + program.register.getValue(v)
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class Multiply(r: Char, v: Long) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> (program.register(r) * v))
-      )
+    data class Multiply(val r: Char, val v: Long) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = program.register.getValue(r) * v
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class MultiplyR(r: Char, v: Char) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> (program.register(r) * program.register(v)))
-      )
+    data class MultiplyR(val r: Char, val v: Char) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = program.register.getValue(r) * program.register.getValue(v)
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class Modulo(r: Char, v: Long) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> (program.register(r) % v))
-      )
+    data class Modulo(val r: Char, val v: Long) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = program.register.getValue(r) % v
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class ModuloR(r: Char, v: Char) extends Operation {
-    def execute(program: Program): Program = {
-      program.copy(
-        counter = program.counter + 1,
-        register = program.register + (r -> (program.register(r) % program.register(v)))
-      )
+    data class ModuloR(val r: Char, val v: Char) : Operation() {
+      override fun execute(program: Program): Program {
+        program.register[r] = program.register.getValue(v) % program.register.getValue(v)
+        return program.copy(
+          counter = program.counter + 1
+        )
+      }
     }
-  }
 
-  case class Receive(r: Char) extends Operation {
-    def execute(program: Program): Program = {
-      if (program.checkRegisterOnReceive) {
-        if (program.register(r) != 0) {
-          Option(program.readChannel.pollLast(1, TimeUnit.SECONDS)) match {
-            case Some(v) => {
-              program.copy(
-                counter = program.counter + 1,
-                register = program.register + (r -> v)
-              )
-            }
-
-            case None => {
+    data class Receive(val r: Char) : Operation() {
+      override fun execute(program: Program): Program {
+        return if (program.checkRegisterOnReceive) {
+          if (program.register.getValue(r) != 0L) {
+            val v = program.readChannel.pollLast(1, TimeUnit.SECONDS)
+            if(v == null) {
               program.copy(
                 counter = program.counter + 1,
                 deadlocked = true
               )
+            } else {
+              program.register[r] = v
+              program.copy(
+                counter = program.counter + 1
+              )
             }
+          } else {
+            program.copy(
+              counter = program.counter + 1
+            )
           }
         } else {
-          program.copy(
-            counter = program.counter + 1
-          )
-        }
-      } else {
-        Option(program.readChannel.pollLast(1, TimeUnit.SECONDS)) match {
-          case Some(v) => {
-            program.copy(
-              counter = program.counter + 1,
-              register = program.register + (r -> v)
-            )
-
-          }
-
-          case None => {
+          val v = program.readChannel.pollLast(1, TimeUnit.SECONDS)
+          if(v == null) {
             program.copy(
               counter = program.counter + 1,
               deadlocked = true
             )
+          } else {
+            program.register[r] = v
+            program.copy(
+              counter = program.counter + 1
+            )
           }
         }
       }
     }
-  }
 
-  case class JumpIfGreaterThanZero(r: Char, v: Int) extends Operation {
-    def execute(program: Program): Program = {
-      if (program.register(r) > 0) {
-        program.copy(
-          counter = program.counter + v
-        )
+    data class JumpIfGreaterThanZero(val r: Char, val v: Int) : Operation() {
+      override fun execute(program: Program): Program {
+        return if (program.register.getValue(r) > 0L) {
+          program.copy(
+            counter = program.counter + v
+          )
+        }
+        else {
+          program.copy(
+            counter = program.counter + 1
+          )
+        }
       }
-      else {
-        program.copy(
-          counter = program.counter + 1
-        )
+    }
+
+    data class JumpIfGreaterThanZeroR(val r: Char, val v: Char) : Operation() {
+      override fun execute(program: Program): Program {
+        return if (program.register.getValue(r) > 0L) {
+          program.copy(
+            counter = program.counter + program.register.getValue(v).toInt()
+          )
+        }
+        else {
+          program.copy(
+            counter = program.counter + 1
+          )
+        }
       }
     }
   }
 
-  case class JumpIfGreaterThanZeroR(r: Char, v: Char) extends Operation {
-    def execute(program: Program): Program = {
-      if (program.register(r) > 0) {
-        program.copy(
-          counter = program.counter + program.register(v).toInt
-        )
+  fun parseInput(input: List<String>): List<Operation> {
+    require(input.isNotEmpty()) { "input.nonEmpty failed" }
+
+    val registerRange = ('a'..'z').toList()
+
+    return input.map { l ->
+      val tokens = l.split(" ")
+      assert(tokens.size >= 2)
+      val operation = tokens[0]
+      val register = tokens[1][0]
+      assert(registerRange.contains(register))
+
+      when(operation) {
+        "snd" -> Operation.Send(register)
+        "set" -> {
+          assert(tokens.size == 3)
+          val operand = tokens[2]
+          if (registerRange.contains(operand[0])) Operation.SetR(register, operand[0])
+          else Operation.Set(register, operand.toLong())
+        }
+        "add" -> {
+          assert(tokens.size == 3)
+          val operand = tokens[2]
+          if (registerRange.contains(operand[0])) Operation.AddR(register, operand[0])
+          else Operation.Add(register, operand.toLong())
+        }
+        "mul" -> {
+          assert(tokens.size == 3)
+          val operand = tokens[2]
+          if (registerRange.contains(operand[0])) Operation.MultiplyR(register, operand[0])
+          else Operation.Multiply(register, operand.toLong())
+        }
+        "mod" -> {
+          assert(tokens.size == 3)
+          val operand = tokens[2]
+          if (registerRange.contains(operand[0])) Operation.ModuloR(register, operand[0])
+          else Operation.Modulo(register, operand.toLong())
+        }
+        "rcv" -> Operation.Receive(register)
+        "jgz" -> {
+          assert(tokens.size == 3)
+          val operand = tokens[2]
+          if (registerRange.contains(operand[0])) Operation.JumpIfGreaterThanZeroR(register, operand[0])
+          else Operation.JumpIfGreaterThanZero(register, operand.toInt())
+        }
+        else -> { assert(false); Operation.Send('a') }
       }
-      else {
-        program.copy(
-          counter = program.counter + 1
-        )
-      }
-    }
+    } //ensuring(_.nonEmpty, s"_.nonEmpty failed")
   }
 
-  def run(program: Program, done: Program => Boolean, exit: Program => Long): Long = {
-    if (done(program)) exit(program)
-    else run(program.instructions(program.counter).execute(program), done, exit)
+  fun run(program: Program, done: (Program) -> Boolean, exit: (Program) -> Long): Long {
+    return if (done(program)) exit(program)
+    else run(program.instructions[program.counter].execute(program), done, exit)
   }
 
   object Part1 {
-    def solve(input: List[String]): Long = {
-      def done(p: Program): Boolean = {
-        if (p.counter < 0 || p.counter >= p.instructions.size) true
-        else p.instructions(p.counter) match {
-          case Receive(r) if (p.register(r) > 0) => true
-          case _ => false
+    fun solve(input: List<String>): Long {
+      fun done(p: Program): Boolean {
+        return if (p.counter < 0 || p.counter >= p.instructions.size) true
+        else when(p.instructions[p.counter]) {
+          is Operation.Receive -> {
+            val r = (p.instructions[p.counter] as Operation.Receive).r
+            if (p.register.getValue(r) > 0L) true else false
+          }
+          else -> false
         }
       }
 
-      def exit(p: Program): Long = {
-        if (p.counter < 0 || p.counter >= p.instructions.size) -1L
+      fun exit(p: Program): Long {
+        return if (p.counter < 0 || p.counter >= p.instructions.size) -1L
         else p.readChannel.pollFirst(5, TimeUnit.SECONDS)
       }
 
       val instructions = parseInput(input)
-      val registers = Map.empty[Char, Long].withDefaultValue(0L)
-      val channel = new LinkedBlockingDeque[Long]()
+      val registers = emptyMap<Char, Long>().toMutableMap().withDefault { 0L }
+      val channel = LinkedBlockingDeque<Long>()
       val program = Program(0, 0, instructions, registers, channel, channel, 0, true, false)
 
-      run(program, done, exit)
+      return run(program, ::done, ::exit)
     }
   }
 
+  @Volatile private var result0 = 0L
+  @Volatile private var result1 = 0L
   object Part2 {
-    def solve(input: List[String]): Long = {
-      def done(p: Program): Boolean = {
-        p.counter < 0 || p.counter >= p.instructions.size || p.deadlocked
+    fun solve(input: List<String>): Long {
+      fun done(p: Program): Boolean {
+        return p.counter < 0 || p.counter >= p.instructions.size || p.deadlocked
       }
 
-      def exit(p: Program): Long = {
-        if(p.deadlocked) p.writeCount else -1L
+      fun exit(p: Program): Long {
+        return if(p.deadlocked) p.writeCount.toLong() else -1L
       }
-
-      import scala.concurrent.ExecutionContext.Implicits.global
-      import scala.concurrent.{Future, Await}
-      import scala.concurrent.duration._
-      import scala.language.postfixOps
 
       val instructions = parseInput(input)
-      val p0Registers = Map.empty[Char, Long].withDefaultValue(0L) + ('p' -> 0L)
-      val p1Registers = Map.empty[Char, Long].withDefaultValue(0L) + ('p' -> 1L)
-      val p0Channel = new LinkedBlockingDeque[Long]()
-      val p1Channel = new LinkedBlockingDeque[Long]()
+      val p0Registers = emptyMap<Char, Long>().toMutableMap().withDefault { 0L }
+      val p1Registers = emptyMap<Char, Long>().toMutableMap().withDefault { 0L }
+      p0Registers['p'] = 0L
+      p1Registers['p'] = 1L
+      val p0Channel = LinkedBlockingDeque<Long>()
+      val p1Channel = LinkedBlockingDeque<Long>()
       val p0 = Program(0, 0, instructions, p0Registers, p1Channel, p0Channel, 0, false, false)
       val p1 = Program(1, 0, instructions, p1Registers, p0Channel, p1Channel, 0, false, false)
 
-      val thread0 = Future { run(p0, done, exit) }
-      val thread1 = Future { run(p1, done, exit) }
-      val result0 = Await.result(thread0, 1 minute)
-      val result1 = Await.result(thread1, 1 minute)
-      result0 + result1
+      val thread0 = kotlin.concurrent.thread() { result0 = run(p0, ::done, ::exit) }
+      val thread1 = kotlin.concurrent.thread() { result1 = run(p1, ::done, ::exit) }
+
+      thread0.join(60*1000)
+      thread1.join(60*1000)
+
+      return result0 + result1
     }
   }
 }
