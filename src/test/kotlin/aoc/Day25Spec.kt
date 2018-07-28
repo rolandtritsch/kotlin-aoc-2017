5e9c6ca68c12b68c1c23c99fe419bde7d91178d2
Roland Tritsch <roland@tritsch.org>
2018-07-28 17:34:17 +0100

    Day25: Spec done.

--- src/test/kotlin/aoc/Day25Spec.kt
+++ src/test/kotlin/aoc/Day25Spec.kt
@@ -1,19 +1,11 @@
-package aoc
+package aoc.day25
 
-import org.scalatest.{FlatSpec, Matchers}
+import io.kotlintest.specs.StringSpec
 
-import scala.collection.mutable
+class Day25Spec : StringSpec({
 
-class Day25Spec extends FlatSpec with Matchers {
-
-  behavior of "run()"
-  it should "produce the right result(s)" taggedAs(BuildTest) in {
-    val tape = Day25.Tape(mutable.ArrayBuffer.fill(101)(0))
-    Day25.run(Day25.StateA(tape.size / 2, tape), 6).checkSum shouldBe 3
-  }
-
-  behavior of "solve() - Part1"
-  it should "solve the puzzle" taggedAs(SolutionTest) in {
-    Day25.Part1.solve(Day25.input) shouldBe 4769
-  }
-}
+  "should solve()" {
+    "part1" {
+      Day25.Part1.solve(Day25.input) shouldBe 4769
+    }
+})
